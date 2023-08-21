589910c6492010846c9b557a196b1dd2da859b6e
PREET <68598635+git-preet@users.noreply.github.com>
2023-08-20 23:00:01 -0400

    Delete deoldify directory

--- deoldify/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import logging
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-logging.getLogger().setLevel(logging.INFO)
-
-from deoldify._device import _Device
-
-device = _Device()
